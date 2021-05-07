d2e5aeb020605cbc487bb383cf78ea4039731063
Fillipe Silva <f.silva@sidi.org.br>
2021-05-06 21:33:20 -0300

    Using same iv

--- src/utils/security-utils.js
+++ src/utils/security-utils.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 const secret = "ExchangePasswordExchangePassword"; // must have 32 bytes
 
 
-const resizedIV = Buffer.allocUnsafe(16);
+const resizedIV = "ExchangePassword"
 
 /**
  * Sign the payload and return the token string
